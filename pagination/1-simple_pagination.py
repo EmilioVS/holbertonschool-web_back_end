3323511a0cc8d514557c45aa65785ac24f60c707
EmilioVS <124622329+EmilioVS@users.noreply.github.com>
2023-09-08 19:33:59 -0600

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,6 +2,7 @@
 '''Description: Implement a method named get_page that takes two integer
                 arguments page with default value 1 and page_size with
                 default value 10.
+
     - You have to use this CSV file (same as the one presented at the top
       of the project)
     - Use assert to verify that both arguments are integers greater than 0.
@@ -11,17 +12,23 @@
     - If the input arguments are out of range for the dataset, an empty list
       should be returned.
 '''
+
 import csv
 import math
 from typing import List
+
 index_range = __import__('0-simple_helper_function').index_range
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
         ''' Initialize instance. '''
         self.__dataset = None
+
     def dataset(self) -> List[List]:
         """Cached dataset
         """
@@ -30,14 +37,18 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         ''' Return page of dataset. '''
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
+
         indices = index_range(page, page_size)
         start = indices[0]
         end = indices[1]
+
         try:
             return self.dataset()[start:end]
         except IndexError:
