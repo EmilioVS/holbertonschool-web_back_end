f4fd50e8d074e4bc531b84acfae2548d3a3ea3f7
EmilioVS <124622329+EmilioVS@users.noreply.github.com>
2023-09-08 19:36:33 -0600

    Update 2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-'''Description: Implement a method named get_page that takes two integer
-                arguments page with default value 1 and page_size with
-                default value 10.
+''' Description: Implement a get_hyper method that takes the same arguments
+                 (and defaults) as get_page and returns a dictionary containing
+                 the following key-value pairs:
 
-    - You have to use this CSV file (same as the one presented at the top
-      of the project)
-    - Use assert to verify that both arguments are integers greater than 0.
-    - Use index_range to find the correct indexes to paginate the dataset
-      correctly and return the appropriate page of the dataset (i.e. the
-    correct list of rows).
-    - If the input arguments are out of range for the dataset, an empty list
-      should be returned.
+        - page_size: the length of the returned dataset page
+        - page: the current page number
+        - data: the dataset page (equivalent to return from previous task)
+        - next_page: number of the next page, None if no next page
+        - prev_page: number of the previous page, None if no previous page
+        -total_pages: the total number of pages in the dataset as an integer
+    Make sure to reuse get_page in your implementation.
+    You can use the math module if necessary.
 '''
 
 import csv
-import math
+from math import ceil
 from typing import List
 
 index_range = __import__('0-simple_helper_function').index_range
@@ -53,3 +53,25 @@ class Server:
             return self.dataset()[start:end]
         except IndexError:
             return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        ''' Return dict of pagination data.
+            Dict key/value pairs consist of the following:
+                page_size - length of dataset page
+                page - current page number
+                data - dataset page
+                next_page - number of next page if there is one
+                prev_page - number of previous page if there is one
+                total_pages - total number of pages '''
+        page_data = self.get_page(page, page_size)
+        total_data = len(self.dataset())
+        total_pages = ceil(total_data / page_size)
+
+        return {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page != 1 else None,
+            'total_pages': total_pages
+        }
